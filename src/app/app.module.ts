8ec326948de803263e40e6f22dca48c3f5b750be
acastef <stefkic.jr@gmail.com>
2019-02-05 06:46:41 +0100

    Podesen ruter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'welcome', component: WelcomeComponent},
+      {path: 'signup', component: SignupComponent},
+      {path: '', redirectTo: '/welcome', pathMatch: 'full'},
+      { path: '**', component: WelcomeComponent, pathMatch : 'full'}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
